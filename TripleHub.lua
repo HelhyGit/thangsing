0144026b31632949c84051afdd3b3c5374d00434
HelhyGit <levygabrieel2025@gmail.com>
2025-06-14 21:15:35 -0300

    Create TripleHub.lua

---
+++ TripleHub.lua
@@ -0,0 +1,20 @@
+local HttpService = game:GetService("HttpService")
+local PlaceId = game.PlaceId
+
+local UniverseID
+do
+    local success, result = pcall(function()
+        return HttpService:JSONDecode(game:HttpGet("https://apis.roblox.com/universes/v1/places/" .. PlaceId .. "/universe"))
+    end)
+
+    if success and result and result.universeId then
+        UniverseID = result.universeId
+    end
+end
+
+if PlaceId == 7449423635 or PlaceId == 2753915549 or PlaceId == 4442272183 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HelhyGit/thangsing/refs/heads/main/BF-Triple.lua"))()
+
+elseif UniverseID == 10260193230 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HelhyGit/thangsing/refs/heads/main/MMS-Triple.lua"))()
+end
