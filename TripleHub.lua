92741a75231309b826a3cd0dea00cb925ae1baa6
HelhyGit <levygabrieel2025@gmail.com>
2025-06-15 10:12:21 -0300

    Update TripleHub.lua

--- TripleHub.lua
+++ TripleHub.lua
@@ -1,12 +1,14 @@
+local HttpService = game:GetService("HttpService")
+local placeId = game.PlaceId
+
 local UniverseID = HttpService:JSONDecode(
     game:HttpGet("https://apis.roblox.com/universes/v1/places/" .. placeId .. "/universe")
 ).universeId
 
-local HttpService = game:GetService("HttpService")
-local placeId = game.PlaceId
-
 if placeId == 7449423635 or placeId == 2753915549 or placeId == 4442272183 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HelhyGit/thangsing/refs/heads/main/BF-Triple.lua"))()
 elseif UniverseID == 10260193230 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HelhyGit/thangsing/refs/heads/main/MMS-Triple.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HelhyGit/thangsing/refs/heads/main/Memesea.lua"))()
+else
+    print("Game not supported by Triple Hub scripts.")
 end
